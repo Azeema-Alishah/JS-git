ebb4cfdebc2e2803d91587d5cc8926f935f3620d
Azeema Alishah <azeemaa.abid@gmail.com>
2023-04-06 10:09:35 -0700

    FEAT: Initialize Project

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+document.write("<h2>Pakistan</h2>")
+
+var name =prompt("YOUR NAME PLS")
+var age2 =prompt("YOUR AGE PLS")
+console.log(name+age2)
+
+console.log('abc');
+console.log(1);
+console.log(true);
+
+console.log("I love JS");
+
+const greet = 'Hello';
+const intro= 'Sara';
+
+console.log(greet + ' ' + intro);
